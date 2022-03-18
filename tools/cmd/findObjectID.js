bdaffea99445c0c65c343dab7feae9b1702cbc1e
Jérôme Cohonner <jerome@cohonner.com>
2022-03-18 17:27:49 +0100

    new cli tool fond Object ID

---
+++ tools/cmd/findObjectID.js
@@ -0,0 +1,58 @@
+const AlgocliScript = require.main.require('./src/algocliScript');
+
+module.exports = class findObjectID extends AlgocliScript {
+    /**
+     * constructor
+     */
+    constructor(algoliaClient, optionString, index) {
+        // add here your default options values
+        // it will be used when you use the command without options
+        let defaultOptionValues = {};
+        super(algoliaClient,index,defaultOptionValues,optionString);
+    }
+
+
+    /**
+     * run function (mandatory)
+     * available variables:
+     * - this.options: contains your command options
+     * - this.client: Algolia client instance
+     * - this.index: Algolia Index instance
+     */
+    async run() {
+        //Looking for product_1200272
+        //product_374491
+        const searchForObjectID = 'product_1200272';
+
+        const query= 'coasters';
+        let parameters = {
+            hitsPerPage: 500,
+            page:0,
+            filters: '',
+            tagFilters: ['product','United States'],
+            distinct:false,
+            attributesToRetrieve:['objectID']
+        };
+        let result = {};
+
+        do {
+            console.log('page: ' + parameters.page);
+
+            result =  await this.index.search(query,parameters);
+
+            if (parameters.page === 0) {
+                console.log('total hits: ' + result.nbHits);
+                console.log('total pages: ' + result.nbPages);
+            }
+
+            const found = result.hits.findIndex(hit => hit.objectID === searchForObjectID);
+            if (found>=0) {
+                console.log('found: ' + searchForObjectID + ' at position: ' + (parameters.page*500+found+1));
+                return;
+            }
+            parameters.page++;
+        } while (parameters.page <= result.nbPages);
+
+        console.log('Object ID not found');
+    }
+}
